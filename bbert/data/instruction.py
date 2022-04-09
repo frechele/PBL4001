90cef3596b0f6ff3bf18d09ce8016016c48c21a7
Junyeong Park <jyp10987@gmail.com>
2022-04-09 16:20:12 +0900

    fix: add methods to Vocabulary

--- bbert/data/instruction.py
+++ bbert/data/instruction.py
@@ -29,4 +29,14 @@ class Vocabulary:
         sep_idx = len(self.inst2ind)
         for i, tok in enumerate(['[SEP]', '[CLS]', '[PAD]', 'MASK'], sep_idx):
             self.inst2ind[tok] = i
-            self.ind2inst[i] = tok
+            self.ind2inst[i] = tok    
+
+    def get_index(self, inst: str) -> Union[int, None]:
+        return self.inst2ind.get(inst, None)
+
+    def get_inst(self, idx: int) -> Union[str, None]:
+        return self.ind2inst.get(idx, None)
+
+    @property
+    def size(self) -> int:
+        return len(self.ind2inst)
