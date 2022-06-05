5a34874a4698375e2253a40409f6cd470e9afb0f
starga2er <jelarum@gmail.com>
2022-06-06 00:59:40 +0900

    add SVC

--- bbert/model/statistics.py
+++ bbert/model/statistics.py
@@ -1,10 +1,19 @@
 import numpy as np
 from typing import List
 from scipy.stats import norm
+from sklearn.svm import SVC
 
 import torch
 import os
 import pickle
+import glob
+
+from bbert.data.instruction import Vocabulary, InstructionMapping
+from bbert.data.dataset import MalwareDataset
+from bbert.model.bbert import BBERT
+
+DATASET_PATH = '/data/pbl/data/pkl2'
+DATA_LIST = glob.glob(os.path.join(DATASET_PATH, '*.pkl'))
 
 MU = 0.5
 SIGMA = 1
@@ -38,17 +47,75 @@ class BBWModel:
         else :
             self.mu = MU
             self.sigma = SIGMA
+        self.predict_model = self.bulid_predict_model()
 
-    def calc_score(self, bbs: List[np.array]):
-        bbs_length = len(bbs)
-        sum = 0
-        weight_list = []
+    def _get_weights(self ,bbs_length):
+        if bbs_length > 1:
+            idxs = np.arange(bbs_length) / (bbs_length - 1)
+        else:
+            idxs = np.array([1.])
+
+        # weights = norm.pdf(idxs, self.mu, self.sigma)
+        weights = np.ones((bbs_length))
+        weights_sum = np.sum(weights)
+        
+        return weights , weights_sum
+
+    def calc_score(self, bbs: List[str]):
+
+        imap = InstructionMapping()
+        vmap = Vocabulary(imap)
+
+        model = BBERT(vmap).cuda()
+        model.load_state_dict(torch.load('bbert.pth'))
+        model = model.bert
+        model.eval()
+
+        sep_id = vmap.get_index('[SEP]')
+        cls_id = vmap.get_index('[CLS]')
+
+        bert_bbs = []
+
         for idx, bb in enumerate(bbs):
-            n = idx/bbs_length
-            weight =norm.pdf(n-self.mu, self.mu, self.sigma)
-            weight_list.append(weight)
-            sum += weight
-        
-        ret = np.array(weight_list) * np.array(bbs) / sum
-        return ret
-        
+            bb = [vmap.get_index(inst) for inst in bb]
+            bb = torch.cat([
+                torch.tensor([cls_id]),
+                torch.tensor(bb).long().contiguous(),
+                torch.tensor([sep_id])
+            ]).long().contiguous()
+
+            bb = bb.unsqueeze(0).cuda()
+            bb = model(bb)
+            bb = bb.cpu().numpy()[0, 0, :]
+            bert_bbs.append(bb)
+
+        total_length = len(bert_bbs)
+        weights , weights_sum = self._get_weights(total_length)
+
+        weighted_bbs = weights * np.array(bert_bbs) / weights_sum
+        return self.predict_model.predict(weighted_bbs.tolist())
+    
+    def bulid_predict_model(self):
+        bb_types = np.zeros(len(DATA_LIST))
+        bbs = np.zeros((len(DATA_LIST), 768))
+
+        for i, filename in enumerate(DATA_LIST):
+            filetype, pe = self.get_program_encoding(filename)
+
+            bb_types[i] = filetype
+            bbs[i] = pe
+
+        return SVC(kernel='linear').fit(bbs, bb_types)
+
+    def get_program_encoding(self, filename: str):
+        with open(filename, 'rb') as f:
+            data = pickle.load(f)
+
+        filetype = data['type']
+        total_length = len(data['bbs'])
+
+        weights, weights_sum = self._get_weights(total_length)
+        bb = np.array(data['bbs'])
+        pe = np.sum(bb * weights[..., np.newaxis], axis=0)
+
+        return filetype, pe / weights_sum
--- inference.py
+++ inference.py
@@ -10,18 +10,17 @@ from bbert.model.statistics import BBLModel
 from bbert.model.statistics import BBWModel
 from bbert.utils.bb_splitter import split_into_bbs
 
-from bbert.data.instruction import Vocabulary, InstructionMapping
-from bbert.data.dataset import MalwareDataset
-from bbert.model.bbert import BBERT
-
 class Inference:
     def __init__(self):
-        # self.bbl = BBWModel('bb_stat.npy')
-        self.bbl = BBWModel('')
+        self.bbl = BBLModel('bb_stat.npy')
+        self.bbw = BBWModel('')
 
     def analyze(self, bbs):
         bbl_score = self.bbl.calc_score(bbs)
         logging.info("BBL score: {}".format(bbl_score))
+
+        predict_filetype = self.bbw.calc_score(bbs)
+        logging.info("BBW predict filetype is {}".format(predict_filetype))
 
 
 def run_one_file(target_filename, inference: Inference):
@@ -36,32 +35,7 @@ def run_one_file(target_filename, inference: Inference):
     bbs = split_into_bbs(instructions)
     logging.info('the number of basic blocks: {}'.format(len(bbs)))
 
-    imap = InstructionMapping()
-    vmap = Vocabulary(imap)
-
-    model = BBERT(vmap).cuda()
-    model.load_state_dict(torch.load('bbert.pth'))
-    model = model.bert
-    model.eval()
-
-    sep_id = vmap.get_index('[SEP]')
-    cls_id = vmap.get_index('[CLS]')
-
-    bert_bbs = []
-
-    for idx, bb in enumerate(bbs):
-        bb = [vmap.get_index(inst) for inst in bb]
-        bb = torch.cat([
-            torch.tensor([cls_id]),
-            torch.tensor(bb).long().contiguous(),
-            torch.tensor([sep_id])
-        ]).long().contiguous()
-        bb = bb.unsqueeze(0).cuda()
-        bb = model(bb)
-        bb = bb.cpu().numpy()[0, 0, :]
-        bert_bbs.append(bb)
-
-    inference.analyze(bert_bbs)
+    inference.analyze(bbs)
 
 
 def main(args):
